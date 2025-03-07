a26c27029593ef21cde4564055fbc42086f013d3
Arkan <105828063+ARKANnnur@users.noreply.github.com>
2025-03-07 20:27:49 +0700

    Update tugas.py

--- tugas.py
+++ tugas.py
@@ -1,1 +1,10 @@
-tes
+from datetime import datetime
+
+tahunSekarang = datetime.now().year
+
+nama = input("Masukkan nama Anda: ")
+tahunLahir = int(input("Masukkan tahun lahir Anda: "))
+
+umur = tahunSekarang - tahunLahir
+
+print(f"Halo, {nama}! Anda berusia {umur} tahun.")
